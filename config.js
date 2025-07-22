d2e9ed52a403be0784c46fdb0809b3258355e600
CuneytD <cuneytdemirturk@gmail.com>
2025-07-22 18:21:55 +0300

    mysql eklendi

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db : {
+        host: "localhost",
+        user: "root",
+        password: "003400",
+        database: "noderdb"
+    }
+}
+
+module.exports = config;
--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const db = require('./data/db');
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -9,8 +11,15 @@ app.get("/index", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-  res.render('index');
-});
+  db.execute('SELECT * FROM products')
+      .then(result => {
+          console.log(result);
+
+          res.render('index');
+        });
+      })
+      // .catch(err => consele.log(err));
+  
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
